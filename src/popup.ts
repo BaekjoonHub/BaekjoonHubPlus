b9aabf3d1cbd78f0e80777ec204f67404b2b4fa8
getCurrentThread <wak8835@gmail.com>
2022-08-24 17:40:07 +0900

    feat: add filesaver, jszip library

--- src/popup.ts
+++ src/popup.ts
@@ -3,12 +3,12 @@ import Config from './repository/config/Config';
 import $ from 'jquery';
 
 $(function () {
-  // 설정 불러와서 #use 체크
-  Config.loadSettings().then(function (settings) {
-    if (settings.use) {
-        (document.getElementById("use") as HTMLInputElement).checked = true;
-    }
-  });
+  const use_func = (settings: any) => {
+    (document.getElementById('use') as HTMLInputElement).checked = settings.use;
+  };
+
+  Config.loadSettings().then(use_func);
+  Config.bind(use_func);
 
   $('#use').on('click', function () {
     Config.setValue('use', $(this).is(':checked'));
